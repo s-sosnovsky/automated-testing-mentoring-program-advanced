4e0f6795497f27e8b1bfcd27b95de5ff96d40e6d
Sergey Sosnovsky <serge.sosnovsky@gmail.com>
2022-05-09 19:29:57 +0300

    Update browser.js

--- test/wrapper/browser.js
+++ test/wrapper/browser.js
@@ -1,19 +1,7 @@
 require("dotenv").config();
 
 const { multiremote } = require("webdriverio");
-const FiredBrowser = require("./browser.singleton");
 const dockerConfig = require('./browser.docker.config')
-
-/**
- * wdio runner config
- */
-// const browserConfiguration = async () => {
-//   return new FiredBrowser(await multiremote(await Promise.resolve(dockerConfig))).browser
-// }
-//  const rawBrowser = browserConfiguration();
-
-
-// module.exports = rawBrowser;
 
 const browserConfiguration = async () => {
   return multiremote(await Promise.resolve(dockerConfig))
