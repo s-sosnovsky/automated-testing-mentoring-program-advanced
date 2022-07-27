732769993dd3172c36c3f6b75159cbf984898d81
Sergey Sosnovsky <sergey_sosnovsky@epam.com>
2022-07-28 01:13:20 +0300

    slack integrations added

--- features/support/steps.js
+++ features/support/steps.js
@@ -1,18 +1,22 @@
 const { When, Then, Before, After } = require("cucumber");
 const logger = require("../../test/utils/helper/logger/logger.config");
+const SlackService = require("../../integrations/SlackService")
 
 const PageFactory = require("../../test/model/pageobjects/playwright/pageFactory");
 const { assert } = require("chai");
 let pageFactory;
+const slackService = new SlackService();
 
-Before({ timeout: 30 * 1000 }, async function () {
+Before({ timeout: 30 * 1000 }, async function (scenario) {
   await this.setDefaultPage();
   pageFactory = new PageFactory(this.page);
   await pageFactory.loginPage.login("default", "1q2w3e");
+  slackService.sendMessage(`${await scenario.pickle.name}: test started`)
 });
 
 After({ timeout: 30 * 1000 }, async function () {
   await pageFactory.homePage.logout();
+  slackService.sendMessage(`${await scenario.pickle.name}: test ended`)
 });
 
 When("Bob goes to launches", { timeout: 30 * 1000 }, async function () {
